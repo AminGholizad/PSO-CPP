36c24e6e065e3a963c3fa98e8dc84eb81c47775a
Amin Gholizad <amin.gholizad@gmail.com>
2025-11-07 10:32:19 +0100

    edited to have better variable names and other changes formore readablity and folowing cpp core giudlines. A concept is added for more clear template types.

--- rand.hpp
+++ rand.hpp
@@ -1,28 +1,32 @@
 #ifndef RAND
 #define RAND
-#include <random>
-#include <algorithm>
 #include <chrono>
-namespace rnd{
-  inline std::mt19937& Generator(){
-    static std::random_device rd;
-    static unsigned seed = (rd.entropy()==0)?std::chrono::system_clock::now().time_since_epoch().count():rd();
-    static std::mt19937 gen(seed);
-    return gen;
-  }
-  template <typename T>
-  inline T unifrnd(T a,T b){
-    if constexpr(std::is_integral_v<T>){
-      std::uniform_int_distribution<T> dis(a, b);
-      return dis(Generator());
-    }
-    else {
-      std::uniform_real_distribution<T> dis(a, b);
-      return dis(Generator());
-    }
-  }
-  inline double rand(){
-    return unifrnd(0.,1.);
+#include <random>
+namespace rnd {
+[[nodiscard]] constexpr std::mt19937 &Generator() {
+  static std::random_device random_device;
+  static unsigned seed =
+      (random_device.entropy() == 0)
+          ? static_cast<unsigned>(
+                std::chrono::system_clock::now().time_since_epoch().count())
+          : random_device();
+  static std::mt19937 gen(seed);
+  return gen;
+}
+
+template <typename T>
+concept NumericType = std::integral<T> || std::floating_point<T>;
+
+template <NumericType T>
+[[nodiscard]] constexpr T unifrnd(T min_val, T max_val) {
+  if constexpr (std::is_integral_v<T>) {
+    std::uniform_int_distribution<T> dis(min_val, max_val);
+    return dis(Generator());
+  } else {
+    std::uniform_real_distribution<T> dis(min_val, max_val);
+    return dis(Generator());
   }
 }
-#endif
+[[nodiscard]] constexpr double rand() { return unifrnd(0.0, 1.0); }
+} // namespace rnd
+#endif // RAND
